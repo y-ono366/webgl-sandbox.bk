03dbf975e7bf0d0d696efebc6dce4b23d5121477
yusuke ohno <yusuke@ohno.systems>
2019-08-12 16:26:31 +0900

    update three js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RotateBox/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RotateBox from './components/RotateBox'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      RotateBox
   }
 }
 </script>
