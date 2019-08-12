ddff4fb06b9773c0380cb2816097f7313774bc69
yusuke ohno <yusuke@ohno.systems>
2019-08-12 21:38:51 +0900

    update sample

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -11,8 +11,13 @@ export default {
     const width = window.innerWidth;
     const height = window.innerHeight;
     const renderer = null;
+    const geometry = new THREE.OctahedronGeometry(1, 0);
+    const material = new THREE.MeshNormalMaterial({ color: 0x6699ff });
+    const mesh = new THREE.Mesh(geometry, material);
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
     const scene = new THREE.Scene();
-    return { scene, width, height, renderer };
+    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    return { scene, width, height, renderer, camera, mesh, directionalLight };
   },
 
   mounted() {
@@ -21,7 +26,20 @@ export default {
       canvas: $canvas,
     });
     this.renderer.setSize(this.width, this.height);
+    this.camera.position.set(0, 0, 4);
+    this.scene.add(this.mesh);
+    this.directionalLight.position.set(1, 1, 1);
+    this.scene.add(this.directionalLight);
+
+    this.tick();
   },
-  methods: {},
+  methods: {
+    tick() {
+      this.mesh.rotation.y += 0.02;
+      this.mesh.rotation.x += 0.01;
+      this.renderer.render(this.scene, this.camera);
+      window.requestAnimationFrame(this.tick);
+    },
+  },
 };
 </script>
