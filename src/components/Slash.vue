870439cbeb218fefcf091cbdbb471c373400a644
yusuke ohno <yusuke@ohno.systems>
2019-08-17 00:28:34 +0900

    add flash

--- src/components/Slash.vue
+++ src/components/Slash.vue
@@ -5,6 +5,7 @@
 <script>
 import * as THREE from 'three';
 import AssetsImage from '@/assets/Smoke-Element.png';
+import StoneImage from '@/assets/stone.png';
 
 export default {
   name: 'Slash',
@@ -26,6 +27,7 @@ export default {
 
     const camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
     const smokeTexture = THREE.ImageUtils.loadTexture(AssetsImage);
+    const stoneTexture = THREE.ImageUtils.loadTexture(StoneImage);
     const smokeMaterial = new THREE.MeshLambertMaterial({
       color: 0xffffff,
       map: smokeTexture,
@@ -34,6 +36,7 @@ export default {
     });
     const light = new THREE.DirectionalLight(0xffffff, 0.5);
     const clock = new THREE.Clock();
+    const flash = new THREE.PointLight(0x062d89, 30, 500, 1.7);
 
     let smokeParticles = [];
     return {
@@ -49,7 +52,8 @@ export default {
       light,
       faces,
       clock,
-      smokeTexture,
+      flash,
+      stoneTexture,
     };
   },
 
@@ -68,7 +72,7 @@ export default {
       this.geometry.faces.push(this.faces[j]);
     }
 
-    const face_material = new THREE.MeshStandardMaterial({ color: 0xff0000, map: this.smokeTexture });
+    const face_material = new THREE.MeshStandardMaterial({ /* color: 0xff0000, */ map: this.stoneTexture });
     const face_mesh = new THREE.Mesh(this.geometry, face_material);
     const face_mesh2 = new THREE.Mesh(this.geometry, face_material);
     const face_mesh3 = new THREE.Mesh(this.geometry, face_material);
@@ -96,6 +100,9 @@ export default {
       this.smokeParticles.push(particle);
     }
 
+    this.flash.position.set(0, 0, 1);
+    this.scene.add(this.flash);
+
     this.light.position.set(0, 0, 1);
     this.scene.add(this.light);
 
@@ -107,12 +114,18 @@ export default {
       this.renderer.render(this.scene, this.camera);
       this.delta = this.clock.getDelta();
       this.evolveSmoke();
+      this.flashAnimation();
     },
     evolveSmoke() {
       let sp = this.smokeParticles.length;
       while (sp--) {
         this.smokeParticles[sp].rotation.z += this.delta * 0.15;
-        //this.smokeParticles[sp].rotation.z += 0.002;
+      }
+    },
+    flashAnimation() {
+      if (Math.random() > 0.97 || this.flash.power > 100) {
+        if (this.flash.power < 100) this.flash.position.set(Math.random() * 200 - 100, Math.random() * 200 - 100, 1);
+        this.flash.power = 20 + Math.random() * 400;
       }
     },
   },
